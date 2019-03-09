f448aeabeb62efeb327a54957765f1fa2c72419d
Elliott Morris <elliottmorris848@gmail.com>
2019-03-09 13:35:42 -0500

    re-calculating the presidential election results for each year because NY & MN list the Dems/Reps in multiple parties

--- ext/scripts/wrangle_mit_elections_pres.R
+++ ext/scripts/wrangle_mit_elections_pres.R
@@ -1,18 +1,33 @@
 library(tidyverse)
-pres <- read_tsv("~/Downloads/1976-2016-president.tab.tsv")
+pres <- read_csv("ext/raw_data/1976-2016-president.csv")
 
 pres <- pres %>%
-  select(-c(state,state_fips,state_cen,state_ic,office,candidate,version,notes))
+  select(-c(state,state_fips,state_cen,state_ic,office,version,notes))
 
 pres <- pres %>%
   dplyr::filter(writein==FALSE) %>%
   dplyr::select(-c(writein))
 
-pres <- pres %>%
-  mutate(party = case_when(party %in% c("democrat","democratic-farmer-labor")~"dem",
-                           party=="republican"~"rep",
-                           TRUE ~ "other"))
+# get the name of the Dem/Rep for each year -- we use DC as the determinant
+dems <- pres %>%
+  filter(party=='democrat',state_po=='DC') %>%
+  group_by(year) %>%
+  summarise(candidate = first(candidate)) %>% pull(candidate)
 
+reps <- pres %>%
+  filter(party=='republican',state_po=='DC') %>%
+  group_by(year) %>%
+  summarise(candidate = first(candidate)) %>% pull(candidate)
+
+
+# assign new parties
+pres <- pres %>%
+  mutate(party = case_when(candidate %in% dems~"dem",
+                           candidate %in% reps~"rep",
+                           TRUE ~ "other")) %>%
+  select(-candidate)
+
+# get votes by party
 pres <- pres %>%
   group_by(year,state_po,party) %>%
   summarise(candidatevotes=sum(candidatevotes),
